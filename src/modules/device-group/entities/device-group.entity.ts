aea5936e975f2699e6422b556faf75be248e39e4
hashbk <littlefishyu2008@gmail.com>
2026-04-11 01:04:58 +0000

    feat(device-group): add entities and peer service
    Define DeviceGroup, DeviceGroupUserPermission, UserUserPermission entities with relationships, add peer management service

---
+++ src/modules/device-group/entities/device-group.entity.ts
@@ -0,0 +1,57 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany, Index } from 'typeorm';
+import { DeviceGroupUserPermission } from './device-group-user-permission.entity';
+
+/**
+ * 设备组实体
+ * 管理设备分组信息
+ */
+@Entity('device_groups')
+export class DeviceGroup {
+  /**
+   * 设备组唯一标识符
+   * UUID格式，用于唯一标识一个设备组
+   */
+  @PrimaryColumn()
+  guid: string;
+
+  /**
+   * 设备组名称
+   * 用于显示和区分不同的设备组
+   */
+  @Column({ unique: true })
+  @Index()
+  name: string;
+
+  /**
+   * 备注
+   * 设备组的详细说明信息
+   */
+  @Column({ type: 'text', nullable: true })
+  note: string;
+
+  /**
+   * 设备组的用户权限列表
+   * 一对多关系，关联到 DeviceGroupUserPermission
+   */
+  @OneToMany(() => DeviceGroupUserPermission, permission => permission.deviceGroup, { cascade: true })
+  userPermissions: DeviceGroupUserPermission[];
+
+  /**
+   * 设备组中的设备列表
+   * 一对多关系，关联到 Peer
+   */
+  @OneToMany('Peer', 'deviceGroup', { cascade: true })
+  peers: any[];
+
+  /**
+   * 创建时间
+   */
+  @CreateDateColumn()
+  createdAt: Date;
+
+  /**
+   * 更新时间
+   */
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
