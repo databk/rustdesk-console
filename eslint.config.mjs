f80ceabedf24f3eecf57112e6605473753b004f9
hashbk <littlefishyu2008@gmail.com>
2026-07-31 16:03:35 +0000

    style(eslint): 关闭unbound-method规则
    关闭该ESLint规则以避免不必要的代码检查警告

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -55,6 +55,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/unbound-method': 'off',
     },
   },
 );
