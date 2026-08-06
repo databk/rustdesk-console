52da2f5594d933f8f309c8087907ce285919aa2e
hashbk <littlefishyu2008@gmail.com>
2026-08-06 17:22:34 +0800

    fix(lint): lint

--- src/modules/update-check/update-check.service.ts
+++ src/modules/update-check/update-check.service.ts
@@ -28,7 +28,6 @@ import { OidcProvider } from '../oidc/entities/oidc-provider.entity';
 import { NexusBuild } from '../nexus/entities/nexus-build.entity';
 import { NexusToken } from '../nexus/entities/nexus-token.entity';
 import { ActiveConnection } from '../heartbeat/entities/active-connection.entity';
-
 import { resolveAssetPath } from '../../common/utils/runtime-paths';
 import {
   UpdateChannel,
